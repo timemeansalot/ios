69d38ff1c6a3027b47b57cad678ced68133d5720
timemeansalot <timemeansalot@gmail.com>
2022-07-09 08:53:00 +0800

    A Swift Tour

--- LearnSwiftLanguage/SwiftBasic.playground/Contents.swift
+++
@@ -1,7 +0,0 @@
-/*:
- # This is heading
- 1. we can change value of var, we can't change value of `let`
- */
-var name: String = "Fu Jie"
-
-print("hello \(name)")
---
+++ LearnSwiftLanguage/SwiftBasic.playground/Pages/ASwiftTour.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//:# This Guide is leart from [A Swfit Tour](https://docs.swift.org/swift-book/GuidedTour/GuidedTour.html)
+
+import Foundation
+
+
+
+//:## `var` vs `let`
+/*:
+ we can change value of `var`, while we can't change value of `let`
+ */
+var age = 12
+let name = "Fu Jie"
+
+print("my age is \(age), my name is \(name)")
+
+
+
